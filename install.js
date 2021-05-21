b5bdd5ac67cd3fbf6ece7de2ad0d8b7a93ee313e
KaKa <kaka@kakawebsitedemo.com>
2021-05-21 18:43:37 +0800

    style: use standard

--- install.js
+++ install.js
@@ -1,14 +1,14 @@
-'use strict';
+'use strict'
 
 //
 // Compatibility with older node.js as path.exists got moved to `fs`.
 //
-var fs = require('fs')
-  , path = require('path')
-  , os = require('os')
-  , hook = path.join(__dirname, 'hook')
-  , root = path.resolve(__dirname, '..', '..')
-  , exists = fs.existsSync || path.existsSync;
+const fs = require('fs')
+const path = require('path')
+const os = require('os')
+const hook = path.join(__dirname, 'hook')
+const root = path.resolve(__dirname, '..', '..')
+const exists = fs.existsSync || path.existsSync
 
 //
 // Gather the location of the possible hidden .git directory, the hooks
@@ -17,43 +17,45 @@ var fs = require('fs')
 // to work correctly.
 //
 
-var git = getGitFolderPath(root);
+let git = getGitFolderPath(root)
+let hooks
+let precommit
 
 // Function to recursively finding .git folder
-function getGitFolderPath(currentPath) {
-  var git = path.resolve(currentPath, '.git')
+function getGitFolderPath (currentPath) {
+  const git = path.resolve(currentPath, '.git')
 
   if (!exists(git) || !fs.lstatSync(git).isDirectory()) {
-    console.log('pre-commit:');
-    console.log('pre-commit: Not found .git folder in', git);
-    
-    var newPath = path.resolve(currentPath, '..');
+    console.log('pre-commit:')
+    console.log('pre-commit: Not found .git folder in', git)
+
+    const newPath = path.resolve(currentPath, '..')
 
     // Stop if we on top folder
     if (currentPath === newPath) {
-      return null;
+      return null
     }
 
-    return getGitFolderPath(newPath);
+    return getGitFolderPath(newPath)
   }
 
-  console.log('pre-commit:');
-  console.log('pre-commit: Found .git folder in', git);
-  return git;
+  console.log('pre-commit:')
+  console.log('pre-commit: Found .git folder in', git)
+  return git
 }
 
 //
 // Resolve git directory for submodules
 //
 if (exists(git) && fs.lstatSync(git).isFile()) {
-  var gitinfo = fs.readFileSync(git).toString()
-    , gitdirmatch = /gitdir: (.+)/.exec(gitinfo)
-    , gitdir = gitdirmatch.length == 2 ? gitdirmatch[1] : null;
+  const gitinfo = fs.readFileSync(git).toString()
+  const gitdirmatch = /gitdir: (.+)/.exec(gitinfo)
+  const gitdir = gitdirmatch.length === 2 ? gitdirmatch[1] : null
 
   if (gitdir !== null) {
-    git = path.resolve(root, gitdir);
-    hooks = path.resolve(git, 'hooks');
-    precommit = path.resolve(hooks, 'pre-commit');
+    git = path.resolve(root, gitdir)
+    hooks = path.resolve(git, 'hooks')
+    precommit = path.resolve(hooks, 'pre-commit')
   }
 }
 
@@ -62,15 +64,15 @@ if (exists(git) && fs.lstatSync(git).isFile()) {
 // triggered. If we do have directory create a hooks folder if it doesn't exist.
 //
 if (!git) {
-  console.log('pre-commit:');
-  console.log('pre-commit: Not found any .git folder for installing pre-commit hook');
-  return;
+  console.log('pre-commit:')
+  console.log('pre-commit: Not found any .git folder for installing pre-commit hook')
+  process.exit(0)
 }
 
-var hooks = path.resolve(git, 'hooks')
-  , precommit = path.resolve(hooks, 'pre-commit');
+hooks = path.resolve(git, 'hooks')
+precommit = path.resolve(hooks, 'pre-commit')
 
-if (!exists(hooks)) fs.mkdirSync(hooks);
+if (!exists(hooks)) fs.mkdirSync(hooks)
 
 //
 // If there's an existing `pre-commit` hook we want to back it up instead of
@@ -78,53 +80,50 @@ if (!exists(hooks)) fs.mkdirSync(hooks);
 // important.
 //
 if (exists(precommit) && !fs.lstatSync(precommit).isSymbolicLink()) {
-  console.log('pre-commit:');
-  console.log('pre-commit: Detected an existing git pre-commit hook');
-  fs.writeFileSync(precommit +'.old', fs.readFileSync(precommit));
-  console.log('pre-commit: Old pre-commit hook backuped to pre-commit.old');
-  console.log('pre-commit:');
+  console.log('pre-commit:')
+  console.log('pre-commit: Detected an existing git pre-commit hook')
+  fs.writeFileSync(precommit + '.old', fs.readFileSync(precommit))
+  console.log('pre-commit: Old pre-commit hook backuped to pre-commit.old')
+  console.log('pre-commit:')
 }
 
 //
 // We cannot create a symlink over an existing file so make sure it's gone and
 // finish the installation process.
 //
-try { fs.unlinkSync(precommit); }
-catch (e) {}
+try { fs.unlinkSync(precommit) } catch (e) {}
 
 // Create generic precommit hook that launches this modules hook (as well
 // as stashing - unstashing the unstaged changes)
 // TODO: we could keep launching the old pre-commit scripts
-var hookRelativeUnixPath = hook.replace(root, '.');
+let hookRelativeUnixPath = hook.replace(root, '.')
 
-if(os.platform() === 'win32') {
-  hookRelativeUnixPath = hookRelativeUnixPath.replace(/[\\\/]+/g, '/');
+if (os.platform() === 'win32') {
+  hookRelativeUnixPath = hookRelativeUnixPath.replace(/[\\/]+/g, '/')
 }
 
-var precommitContent = '#!/usr/bin/env bash' + os.EOL
-  +  hookRelativeUnixPath + os.EOL
-  + 'RESULT=$?' + os.EOL
-  + '[ $RESULT -ne 0 ] && exit 1' + os.EOL
-  + 'exit 0' + os.EOL;
+const precommitContent = '#!/usr/bin/env bash' + os.EOL +
+  hookRelativeUnixPath + os.EOL +
+  'RESULT=$?' + os.EOL +
+  '[ $RESULT -ne 0 ] && exit 1' + os.EOL +
+  'exit 0' + os.EOL
 
 //
 // It could be that we do not have rights to this folder which could cause the
 // installation of this module to completely fail. We should just output the
 // error instead destroying the whole npm install process.
 //
-try { fs.writeFileSync(precommit, precommitContent); }
-catch (e) {
-  console.error('pre-commit:');
-  console.error('pre-commit: Failed to create the hook file in your .git/hooks folder because:');
-  console.error('pre-commit: '+ e.message);
-  console.error('pre-commit: The hook was not installed.');
-  console.error('pre-commit:');
+try { fs.writeFileSync(precommit, precommitContent) } catch (e) {
+  console.error('pre-commit:')
+  console.error('pre-commit: Failed to create the hook file in your .git/hooks folder because:')
+  console.error('pre-commit: ' + e.message)
+  console.error('pre-commit: The hook was not installed.')
+  console.error('pre-commit:')
 }
 
-try { fs.chmodSync(precommit, '777'); }
-catch (e) {
-  console.error('pre-commit:');
-  console.error('pre-commit: chmod 0777 the pre-commit file in your .git/hooks folder because:');
-  console.error('pre-commit: '+ e.message);
-  console.error('pre-commit:');
+try { fs.chmodSync(precommit, '777') } catch (e) {
+  console.error('pre-commit:')
+  console.error('pre-commit: chmod 0777 the pre-commit file in your .git/hooks folder because:')
+  console.error('pre-commit: ' + e.message)
+  console.error('pre-commit:')
 }
