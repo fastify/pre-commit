38e02780e61af8914bbbfbd59a10762abb1a2fa1
Uzlopak <aras.abbasi@googlemail.com>
2023-09-10 11:09:58 +0200

    try to fix tests

--- test.js
+++ test.js
@@ -147,7 +147,7 @@ t.test('pre-commit', function (t) {
       t.plan(1)
 
       const Hook = proxyquire('.', {
-        fs: {
+        'node:fs': {
           existsSync () {
             return true
           },
@@ -168,7 +168,7 @@ t.test('pre-commit', function (t) {
       t.plan(4)
 
       let Hook = proxyquire('.', {
-        fs: {
+        'node:fs': {
           existsSync () {
             return true
           },
@@ -181,7 +181,7 @@ t.test('pre-commit', function (t) {
       hook = new Hook(exit)
 
       Hook = proxyquire('.', {
-        fs: {
+        'node:fs': {
           existsSync () { return true },
           readFileSync () {
             return Buffer.from('{ "bad": [json }')
